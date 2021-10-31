7bdcb72a3c35f582913ec9046d4a73b54999b649
thegulers <selami.gulerr@gmail.com>
2021-11-01 01:02:44 +0300

    01112021

---
+++ intro/src/ProductList.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react"
+
+export default class ProductList extends Component{
+    render(){
+        return(
+            <div>
+               <h1>ProductList</h1> 
+            </div>
+        )
+    }
+}
